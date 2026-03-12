4a06c641dd74c18f0f4cee8bad045c872bc71a21
Sahon Shaha <shahasahon@gmail.com>
2026-03-12 17:47:56 -0400

    Changed DuplicateEmailException to being an unchecked exception

--- src/main/java/Exceptions/DuplicateEmailException.java
+++ src/main/java/Exceptions/DuplicateEmailException.java
@@ -1,6 +1,6 @@
 package Exceptions;
 
-public class DuplicateEmailException extends Exception {
+public class DuplicateEmailException extends RuntimeException {
     // Exception for when the email is already taken
     public DuplicateEmailException() {
         super("The following email address already exists.");
